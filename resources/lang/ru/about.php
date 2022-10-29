b59ea29f68dd74c67106544dc5de5a948212dd30
monosniper <vanteateam@gmail.com>
2022-10-29 18:24:44 +0500

    init

---
+++ resources/lang/ru/about.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'like' => config('app.name').', как',
+    'titles' => [
+        'shop' => [
+            '1' => 'Lorem ipsum dolor sit amet',
+            '2' => 'Lorem ipsum dolor sit amet',
+            '3' => 'Lorem ipsum dolor sit amet',
+        ],
+        'freelance' => [
+            '1' => 'Lorem ipsum dolor sit amet',
+            '2' => 'Lorem ipsum dolor sit amet',
+            '3' => 'Lorem ipsum dolor sit amet',
+        ],
+    ],
+    'descriptions' => [
+        'shop' => [
+            '1' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores maiores mollitia nihil quam quasi tempora temporibus voluptas voluptate.',
+            '2' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores maiores mollitia nihil quam quasi tempora temporibus voluptas voluptate.',
+            '3' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores maiores mollitia nihil quam quasi tempora temporibus voluptas voluptate.',
+        ],
+        'freelance' => [
+            '1' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores maiores mollitia nihil quam quasi tempora temporibus voluptas voluptate.',
+            '2' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores maiores mollitia nihil quam quasi tempora temporibus voluptas voluptate.',
+            '3' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores maiores mollitia nihil quam quasi tempora temporibus voluptas voluptate.',
+        ],
+    ]
+];
---
+++ resources/views/dashboard/freelancer.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dashboard')
+
+@section('title')
+    {{__('main.become_freelancer')}}
+@endsection
+
+@section('content')
+    <div class="grey_block">
+        <div class="profile_title">{{__('main.become_freelancer')}}</div>
+    </div>
+@endsection
